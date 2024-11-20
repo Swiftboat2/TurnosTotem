0ed33bb4a84109ab0601e9abcc318c38b222f769
Swiftboat2 <90280978+Swiftboat2@users.noreply.github.com>
2024-11-20 01:40:42 -0300

    .jsx agregados

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://turnos-totem.vercel.app/',
+    
   }
 });
 
